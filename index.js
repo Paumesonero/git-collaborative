af2e5f0cec753a05272f7c5f2879d23cfc3763a4
Pau Mesonero <paumesonero@Caitlins-MacBook-Air.local>
2022-06-25 11:39:41 +0200

    cool function added

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 function newFunc() {
-    console.log("I'm very new")
+  console.log("I'm very new")
 }
 
 newFunc()
 function megaCool() {
   console.log("I might be the coolest ever")
   return 2 + 2;
+}
+
+function coolFunc() {
+  return 3 + 3
 }
 
 megaCool();
